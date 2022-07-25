e3ae07950181a3af4d56853a20de3532eabf12c1
Jim <104743175+eljimmo@users.noreply.github.com>
2022-07-24 19:34:01 -0500

    Create lookup.py

---
+++ lookup.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame([['1990', 'a', 5, 4, 7, 2], ['1991', 'c', 10, 1, 2, 0], ['1992', 'd', 2, 1, 4, 12], ['1993', 'a', 5, 8, 11, 6]], columns=('Date', 'best', 'a', 'b', 'c', 'd'))
+df
+
+print(df)
