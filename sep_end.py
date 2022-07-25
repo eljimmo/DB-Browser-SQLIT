7944ba81c90bfe28568e9784d1bcc8f73aadfcab
Jim <104743175+eljimmo@users.noreply.github.com>
2022-07-24 19:56:00 -0500

    Create sep_end.py

---
+++ sep_end.py
@@ -0,0 +1,12 @@
+#seperation softspace feature, add in what you want to be the seperator between 
+# the things in your list 
+print('Rate', 'Confirmation', 'Uber', sep=' ')
+
+print('09', '12', '2022', sep = '-')
+
+print('john', 'tql', sep ='@')
+
+#https://www.geeksforgeeks.org/gfact-50-python-end-parameter-in-print/
+#combine two print statements, by ending one with end
+print('jane','doe', sep='', end='@')
+print('xpo')
